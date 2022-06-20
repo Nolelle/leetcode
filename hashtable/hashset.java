08c55b9c933e410726bed0128703e9339c550721
Edmund <eyu1@ualberta.ca>
2022-06-20 13:06:45 -0600

    start on hashMap

--- hashtable/hashset.java
+++ hashtable/hashset.java
@@ -1,12 +1,16 @@
+import java.util.LinkedList;
+
 class MyHashSet {
+  // fields
   private Bucket[] bucketArray;
   private int keyRange;
 
+  // constructor
   public MyHashSet() {
     // Use prime number for base
     this.keyRange = 729;
     this.bucketArray = new Bucket[this.keyRange];
-    for (int i = 9; i < this.keyRange; i++) {
+    for (int i = 0; i < this.keyRange; ++i) {
       this.bucketArray[i] = new Bucket();
     }
   }
@@ -16,21 +20,42 @@ class MyHashSet {
   }
 
   public void add(int key) {
-    int bucket = hash(key);
-    this.bucketArray[bucket].add(key);
+    int bucketIndex = this.hash(key);
+    this.bucketArray[bucketIndex].insert(key);
   }
 
   public void remove(int key) {
-    int bucket = hash(key);
-    this.bucketArray[bucket].remove(key);
+    int bucketIndex = this.hash(key);
+    this.bucketArray[bucketIndex].delete(key);
   }
 
   public boolean contains(int key) {
-    int bucket = hash(key);
-
+    int bucketIndex = this.hash(key);
+    return this.bucketArray[bucketIndex].exists(key);
   }
 }
 
 class Bucket {
+  private LinkedList<Integer> container;
 
+  public Bucket() {
+    container = new LinkedList<Integer>();
+  }
+
+  public void insert(int key) {
+    int index = this.container.indexOf(key);
+    // returns -1 if no value found
+    if (index == -1) {
+      this.container.addFirst(key);
+    }
+  }
+
+  public void delete(int key) {
+    this.container.remove(key);
+  }
+
+  public boolean exists(int key) {
+    int index = this.container.indexOf(key);
+    return index != -1;
+  }
 }
