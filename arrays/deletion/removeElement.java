18c826af38c6d5e2dd947b96de0f40c95874eeb4
Edmund <eyu1@ualberta.ca>
2022-08-05 22:57:20 -0600

    finish removeElement

--- arrays/deletion/removeElement.java
+++ arrays/deletion/removeElement.java
@@ -1,14 +1,17 @@
 package deletion;
 
+// Solution 1: Two Pointers
 class removeElement {
   public int remove(int[] nums, int val) {
-    int k = 0;
-    int r1 = nums.length - 1;
+    int j = 0;
     for (int i = 0; i < nums.length; i++) {
-      if (nums[i] == val) {
-        nums[r1] = nums[i];
+      if (nums[i] != val) {
+        nums[i] = nums[j];
+        j++;
       }
     }
-    return k;
+    return j;
   }
 }
+// Time Complexity: O(n)
+// Space Complexity: O(1)
