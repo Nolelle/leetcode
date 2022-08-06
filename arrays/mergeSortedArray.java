e0548bda14668f9ad358bc1f0f46b813bd1fcf75
Edmund <eyu1@ualberta.ca>
2022-08-05 19:06:55 -0600

    finish mergeSortedArray

--- arrays/mergeSortedArray.java
+++ arrays/mergeSortedArray.java
@@ -1,11 +1,34 @@
+// Solution 1 
+// import java.util.Arrays;
+// public class mergeSortedArray {
+//   public void merge(int[] nums1, int m, int[] nums2, int n) {
+//     for (int i = 0; i < n; i++) {
+//       nums1[i + m] = nums2[i];
+//     }
+//     Arrays.sort(nums1);
+//     return;
+//   }
+// }
+// Time Complexity: O(N) with the Sorting Function in Java O((M+N)log(M+N))
+// Space Complexity: O(1) with the Sorting Function in Java O((log(M+N))
+
+// Solution 2: Merge Sort Algorithm
 public class mergeSortedArray {
   public void merge(int[] nums1, int m, int[] nums2, int n) {
-    for (int i = 0; i < m; i++) {
-      if (nums2[i] >= nums1[i] && nums2[i] < nums1[i + 1]) {
-        nums1[i + 1] = nums2[i];
-      } else if (nums2[i] < nums1[i]) {
-        nums1[i - 1] = nums2[i];
+    int r1 = m - 1;
+    int r2 = n - 1;
+    for (int w = m + n - 1; w >= 0; w--) {
+      if (r1 >= 0 && r2 >= 0) {
+        nums1[w] = nums1[r1] > nums2[r2] ? nums1[r1--] : nums2[r2--];
+      } else if (r1 >= 0) {
+        nums1[w] = nums1[r1--];
+      } else {
+        nums1[w] = nums1[r2--];
       }
     }
+    return;
   }
 }
+
+// Time Complexity = O(M+N)
+// Space Complexity: O(1)
