264f44530a78311e4f8eac1a8839fee6a3613023
Edmund <eyu1@ualberta.ca>
2022-08-08 16:32:38 -0600

    finish removeDuplicates

--- arrays/deletion/removeDuplicates.java
+++ arrays/deletion/removeDuplicates.java
@@ -2,6 +2,22 @@ package deletion;
 
 public class removeDuplicates {
   public int removeDups(int[] nums) {
-    return 1;
+    // j will be use to track of unique elemeents, starts at 1 because we assume
+    // that the first element is unique.
+    int j = 1;
+    int l = nums.length;
+    if (l == 0) {
+      return 0;
+    }
+    for (int i = 1; i < l; i++) {
+      if (nums[i] != nums[i - 1]) {
+        nums[j] = nums[i];
+        j++;
+      }
+    }
+    return j;
   }
 }
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
