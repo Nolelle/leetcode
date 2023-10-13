bc140e08f405ccb5921b7ad51baadb6905f8ca79
Nolelle <eyu1@ualberta.ca>
2023-10-13 00:25:40 -0600

    finish same_tree.py

--- Python /Binary_Trees/binary_tree_general/same_tree.py
+++ Python /Binary_Trees/binary_tree_general/same_tree.py
@@ -13,10 +13,11 @@ class TreeNode:
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         # base case
+        if p is None and q is None:
+            return True
+        if p is None or q is None:
+            return False
+        if p.val != q.val:
+            return False
         # recursive case
-
-        return True
-
-
-# obj = Solution()
-# print(obj.isSameTree(([1, 2, 3]), ([1, 2, 3])))
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
