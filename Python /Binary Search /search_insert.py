439fecac7c03bd5b5835a5a4e08b1eb9c2b6319d
Nolelle <eyu1@ualberta.ca>
2024-04-30 15:33:05 -0600

    working

--- Python /Binary Search /search_insert.py
+++ Python /Binary Search /search_insert.py
@@ -1,10 +1,10 @@
 class Solution:
-    def searchInsert(self, nums: list[int], target: int ) -> int:
+    def searchInsert(self, nums: list[int], target: int) -> int:
         low = 0
         high = len(nums) - 1
-        middle = (low + high) // 2
 
         while low <= high:
+            middle = (low + high) // 2
             print(f"Middle: {nums[middle]}")
             if target == nums[middle]:
                 return middle
@@ -15,9 +15,7 @@ class Solution:
                 high = middle - 1
                 print(f"High {nums[high]}")
 
-        return middle
-
 
 obj = Solution()
 
-print(obj.searchInsert([1,3,5,6], 5))
+print(obj.searchInsert([1, 3, 5, 6], 5))
