1120bb69313ef315f56373c3e886cfe91fc6d3d2
Edmund <eyu1@ualberta.ca>
2025-05-08 11:20:38 -0600

    Refactor moveZeroes function to improve clarity and maintainability

--- Python /Arrays/move-zeroes.py
+++ Python /Arrays/move-zeroes.py
@@ -3,3 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+
+        # Input: nums = [0,1,0,3,12]
+        # Output: [1,3,12,0,0]
+        # have one pointer that tracks the latest non zero element in the array
+        # another pointer that advances throught he array
+
+        n = len(nums)
+        nonzero = 0
+
+        for i in range(n):
+            if nums[i] != 0:
+                nums[nonzero] = nums[i]
+                nonzero += 1
+
+        for j in range(nonzero, n):
+            nums[j] = 0
