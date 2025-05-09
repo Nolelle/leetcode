8775a985bc978dada38e944b94da5c5a2b9c3e07
Edmund <eyu1@ualberta.ca>
2025-05-09 08:07:00 -0600

    Add NumArray class for range sum queries with prefix sum array

---
+++ Python /Arrays/range-sum-query.py
@@ -0,0 +1,26 @@
+class NumArray:
+
+    def __init__(self, nums: list[int]):
+        for num in nums:
+            self.prefix = [0]
+            self.prefix.append(self.prefix[-1] + num)
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.prefix[right + 1] - self.prefix[left]
+
+
+# Input
+# ["NumArray", "sumRange", "sumRange", "sumRange"]
+# [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+# Output
+# [null, 1, -1, -3]
+
+# Explanation
+# NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+# numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+# numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+# numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
