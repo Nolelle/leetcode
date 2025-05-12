02e43ce8a04de63f79ce2efdd0df21add0b036bd
Edmund <eyu1@ualberta.ca>
2025-05-12 09:12:06 -0600

    Refactor findContentChildren method to improve readability and maintainability by adding detailed comments and organizing code structure.

--- Python/Arrays/assign-cookies.py
+++ Python/Arrays/assign-cookies.py
@@ -1,6 +1,24 @@
 class Solution:
     def findContentChildren(self, g: list[int], s: list[int]) -> int:
+        # this seems to be like a O(n) array problem where we have to loop through
+        # the entire array to see if we can assign cookies to children
+        # we want to ensure we try our best to empty the cookies array each time.
+        # output is number of cookies given out
+        # g array = greed factor of each child
+        # s array = size of cookie
+        # condiiton to give out cookie is s[j] >= g[i]
 
+        output = 0
+        g.sort()
+        s.sort()
+        g_pointer, s_pointer = 0, 0
+        ng = len(g)
+        ns = len(s)
 
+        while g_pointer < ng and s_pointer < ns:
+            if s[s_pointer] >= g[g_pointer]:
+                output += 1
+                g_pointer += 1
+            s_pointer += 1
 
-       
+        return output
